15548d3245d5e4938a04e28f974d2680bc4eb00d
fezzc <zhouzuchuan@icloud.com>
2019-01-31 15:55:32 +0800

    v1.0.0 feat:  react-ts-boilerplate 工程

---
+++ boilerplates/react-ts/declarations.d.ts
@@ -0,0 +1,3 @@
+declare module 'react-enhanced'
+declare module 'api-manage'
+declare module 'react-loadable'
---
+++ boilerplates/react-ts/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ident: 'postcss',
+    plugins: {
+        'postcss-cssnext': {},
+        'postcss-import': {},
+        'postcss-url': {},
+    },
+}
